a8a3ef06c24163d9d25bd275d101e54093caf3a1
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-05-30 16:21:43 +0530

    Updated UserEntity with Size and Past validation

--- A-REST-Services/src/main/java/com/rest/user/User.java
+++ A-REST-Services/src/main/java/com/rest/user/User.java
@@ -2,11 +2,19 @@ package com.rest.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
+	
+	@Size(min = 2, message = "Name should be atleast 2 Characters and max 30", max = 30)
 	private String name;
+	
+	@Past
 	private Date birthdate;
+	
 	public Integer getId() {
 		return id;
 	}
