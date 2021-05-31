4be2c967d777e631c1350ca9ed194d492a0ea5c1
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-05-31 14:01:38 +0530

    Update HelloWorldController with LocalContextHolder.getLocale

--- A-REST-Services/src/main/java/com/rest/helloworld/HelloWorldController.java
+++ A-REST-Services/src/main/java/com/rest/helloworld/HelloWorldController.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -38,11 +40,13 @@ public class HelloWorldController {
 	
 	//internationlization
 	@GetMapping(path = "/hello-world-internationalized")
-	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false)Locale locale)
+	public String helloWorldInternationalized(
+			//@RequestHeader(name = "Accept-Language", required = false)Locale locale //Instead of getting Locale everytime - Check line 47
+			)
 	{										//Accepting language from headers and taking locale requestheader
 		//return "Hello World!";
 		
-		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
 		
 		//en - Hellow World
 		//nl - Goede Morgen
