72de22413cc29626bd7b34bf37134fab1b17ae60
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-01 11:59:55 +0530

    PersonV1 returns name as Single field

---
+++ A-REST-Services/src/main/java/com/rest/versioning/PersonV1.java
@@ -0,0 +1,24 @@
+package com.rest.versioning;
+
+public class PersonV1 {//Name with single field
+	
+	private String name;
+
+	public PersonV1() {
+		super();
+	}
+	
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
