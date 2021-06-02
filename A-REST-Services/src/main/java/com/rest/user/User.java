41dd334544c7494614d37a06aef29f533a77560c
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-02 12:56:06 +0530

    Updated User Bean with @Entity and @id @generatedValue for JPA

--- A-REST-Services/src/main/java/com/rest/user/User.java
+++ A-REST-Services/src/main/java/com/rest/user/User.java
@@ -2,11 +2,17 @@ package com.rest.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should be atleast 2 Characters and max 30", max = 30)
