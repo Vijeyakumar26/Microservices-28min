f03ff1b3095391bde7fd935dee1abf15537a8c08
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-03 15:39:39 +0530

    Create LimitsController

---
+++ B-limit-service/src/main/java/com/microservices/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package com.microservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.bean.Limits;
+
+@RestController
+public class LimitsController {
+	
+	@GetMapping(path = "/limits")
+	public Limits retrieveLimits() {
+		return new Limits(1,1000);
+	}
+}
