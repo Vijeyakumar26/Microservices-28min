696b8ccc0cf0fd1226dd9620a7300886e2d332a0
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-03 16:10:57 +0530

    Autowiring Configuration Class

--- B-limit-service/src/main/java/com/microservices/controller/LimitsController.java
+++ B-limit-service/src/main/java/com/microservices/controller/LimitsController.java
@@ -1,15 +1,23 @@
 package com.microservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.bean.Limits;
+import com.microservices.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 	
+	@Autowired
+	private Configuration configuration;
+	
+	
 	@GetMapping(path = "/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());//retrieveing values from application.properties	
+		//return new Limits(1,1000);
 	}
 }
