7b6d6b5904aa7c5aa5c822c5caa1ae7b2948a4fa
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-09 11:29:27 +0530

    Updated with retrieveExchangeValues

--- currency-exchange-service/src/main/java/com/microservices/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/CurrencyExchangeController.java
@@ -1,8 +1,18 @@
 package com.microservices;
 
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to)
+	{
+		return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(50));
+	}
 
 }
