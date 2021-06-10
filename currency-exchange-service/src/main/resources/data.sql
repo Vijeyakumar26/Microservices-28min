962aa1279d2bb8688d367e9e213fbd07e0b34a62
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-10 12:28:33 +0530

    Updated Table Values

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into currency_exchange(id,conversion_multiple,currency_from,currency_to)
-values(10001,'USD','INR',65);
-insert into currency_exchange(id,conversion_multiple,currency_from,currency_to)
-values(10002,'EUR','INR',75);
-insert into currency_exchange(id,conversion_multiple,currency_from,currency_to)
-values(10003,'AUD','INR',25);
+insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
+values(10001,65,'USD','INR','');
+insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
+values(10002,75,'USD','INR','');
+insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
+values(10003,87,'AUD','INR','');
