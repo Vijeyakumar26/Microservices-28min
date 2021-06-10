6e28d021e224f37777a09955b553cab71bd04e55
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-06-10 12:50:34 +0530

    Updated Values

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
 values(10001,65,'USD','INR','');
 insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
-values(10002,75,'USD','INR','');
+values(10002,75,'EUR','INR','');
 insert into currency_exchange(id,conversion_multiple,currency_from,currency_to,environment)
 values(10003,87,'AUD','INR','');
