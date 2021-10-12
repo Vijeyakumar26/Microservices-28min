6ad61dde117c8394d1a8aed168e7f63a9e69931f
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-10-12 22:43:26 +0530

    initial commit

---
+++ server/src/main/java/com/microservices/ServerApplication.java
@@ -0,0 +1,25 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+	
+	
+}
+@RestController
+class Controller{
+	
+	@RequestMapping(path = "/")
+	String reqMapping()
+	{
+		return "Controller";
+	}
+}
